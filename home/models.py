ba408db95613846fa0c8cba92462898acdb93a51
manjeet kumar <kumarmanjeet50728@gmail.com>
2025-08-08 00:39:03 +0530

    complete email connection to frontend

--- home/models.py
+++ home/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 
-# Create your models here.
-
 class Enquiry(models.Model):
-    lead_stage_type = [
+    LEAD_STAGE_CHOICES = [
         ('DROP NOT INTERESTED', 'DROP NOT INTERESTED'),
         ('BLANK', 'BLANK'),
         ('BUSY', 'BUSY'),
@@ -11,25 +9,32 @@ class Enquiry(models.Model):
         ('PROSPECT', 'PROSPECT'),
     ]
 
-    lead_source_type = [
+    LEAD_SOURCE_CHOICES = [
         ('Website', 'Website'),
-        ('Linkdin', 'Linkdin'),
+        ('LinkedIn', 'LinkedIn'),
         ('Instagram', 'Instagram'),
     ]
-    
-    name = models.CharField(max_length=100, null=False),
-    email = models.EmailField(null=False),
-    phone = models.BigIntegerField(null=False),
-    date_of_birth = models.DateField(null=False),
-    university = models.CharField(max_length=100, null=False),
-    course = models.CharField(max_length=100, null=False),
-    lead_source = models.CharField(max_length=100, choices=lead_source_type, default='Website'),
-    lead_stage = models.CharField(max_length=100, choices=lead_stage_type),
 
+    name = models.CharField(max_length=100, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    phone = models.BigIntegerField(null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    university = models.CharField(max_length=100, null=True, blank=True)
+    course = models.CharField(max_length=100, null=True, blank=True)
+    lead_source = models.CharField(
+        max_length=100,
+        choices=LEAD_SOURCE_CHOICES,
+        default='Website',
+        null=True,
+        blank=True
+    )
+    lead_stage = models.CharField(
+        max_length=100,
+        choices=LEAD_STAGE_CHOICES,
+        default='BLANK',
+        null=True,
+        blank=True
+    )
 
-def __str__(self):
-        return self.name
-
-    
-    
-
+    def __str__(self):
+        return self.name if self.name else "Unnamed Enquiry"
